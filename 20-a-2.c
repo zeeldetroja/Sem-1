d2250a1c35cc9489b25694b12c604bb1fcaf4df5
zeeldetroja <140875749+zeeldetroja@users.noreply.github.com>
2023-11-08 08:59:14 +0530

    Add files via upload

---
+++ 20-a-2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+struct book(char ,int ,float ){
+	char name[10];
+	int page no;
+	float price;
+}
+void main(){
+	printf("enter name:");
+	scanf("%s",&book.name[]);
+	printf("enter page no.=");
+	scanf("%d",&book.page no);
+	printf("enter price=");
+	scanf("%f",&book.price);
+}
